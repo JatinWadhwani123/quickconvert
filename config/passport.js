88b3c1ef470808ea1b3e421509d6fc0676b4ab4c
Jatin Wadhwani <jatinwadhwaniofficial1@gmail.com>
2026-03-20 09:06:21 +0530

    Removed facebook strategy completely

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,6 @@
 const passport = require("passport");
 
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
 
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((obj, done) => done(null, obj));
@@ -15,32 +14,6 @@ clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 },
 (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
-}));
-
-/* FACEBOOK */
-
-passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: `${process.env.BASE_URL}/auth/facebook/callback`,
-  profileFields: ['id', 'displayName', 'emails']
-},
-async (accessToken, refreshToken, profile, done) => {
-  try {
-    const email = profile.emails?.[0]?.value || null;
-
-    const user = {
-      facebookId: profile.id,
-      name: profile.displayName,
-      email: email
-    };
-
-    return done(null, user);
-
-  } catch (err) {
-    console.log("FB ERROR:", err);
-    return done(err, null);
-  }
 }));
 
 module.exports = passport;
